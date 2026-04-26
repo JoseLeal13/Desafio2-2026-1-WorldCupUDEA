be85068c060ed25435bc7cc93f5b496570b3f0b5
Emmanuelgt-code <emmanuel.guerrat@udea.edu.co>
2026-04-26 15:27:12 -0500

    Agrego en la carpeta mainWorldCup con la logica añadida de las estadisticas que nos pide el problema las cuales son las confederaciones que mas aparecen en cada fase eliminatoria, los ranking de los semifinalistas (top4), los 3 maximos goleadores del torneo, y el goleador de el equipo campeon

---
+++ PRUEBAS_DESAFIO2/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/CMakeFiles/PRUEBAS_DESAFIO2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PRUEBAS_DESAFIO2.dir/main.cpp.obj"
+  "CMakeFiles/PRUEBAS_DESAFIO2.dir/main.cpp.obj.d"
+  "PRUEBAS_DESAFIO2.exe"
+  "PRUEBAS_DESAFIO2.exe.manifest"
+  "PRUEBAS_DESAFIO2.pdb"
+  "libPRUEBAS_DESAFIO2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PRUEBAS_DESAFIO2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
